80baa94d09dafba5cbce23e28b396de06711e54b
Karthik18v <bittukarthik77@gmail.com>
2024-12-13 12:15:00 +0530

    add new end point

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
+
 
 app.listen(4000, () => console.log(`Server Running At http://localhost:4000`));
 
 app.get("/",async(request,response)=>{
     response.send("Hello");
+})
+
+app.get("/hello",async(request,respose)=>{
+    respose.send("New");
 })
 
 module.exports = app;
